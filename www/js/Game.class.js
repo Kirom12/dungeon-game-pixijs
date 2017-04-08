850235f24989e357db547f6168560d53116fb7ff
Baptiste Rosillon <batros.web@gmail.com>
2017-04-08 21:47:46 +0200

    implemented level on Game class

--- www/js/Game.class.js
+++ www/js/Game.class.js
@@ -23,10 +23,35 @@ class Game {
 		Game.MovieClip = PIXI.extras.MovieClip;
 
 		Game.stage = {
-			width : 1600,
-			height : 900,
-			container : new Game.Container()
+			width : 1400,
+			height : 800,
+			container : new Game.Container(),
+			scaleModifier : 2.5
 		}
+
+		Game.assets;
+		Game.levels = {
+			info : [{
+				name : "Level 1",
+				startX : 400,
+				startY : 750
+			},
+			{
+				name : "Level 2",
+				startX : 400,
+				startY : 770
+			}],
+			urls : [
+				{ name : "mapLvl1", url : "assets/maps/level-1.json"},
+				{ name : "mapLvl2", url : "assets/maps/level-2.json"},
+			]
+		}
+		Game.assetsUrls = [
+			{ name : "dungeon", url : "assets/tilesets/dungeon_tiles.png"},
+			{ name : "collision", url : "assets/tilesets/collision.png"},
+			{ name : "character", url : "assets/tilesets/character.png"},
+			{ name : "enemies", url : "assets/tilesets/enemies.png"}
+		]
 
 		Game.renderer = new Game.autoDetectRenderer(Game.stage.width, Game.stage.height);
 		document.body.appendChild(Game.renderer.view);
@@ -38,7 +63,15 @@ class Game {
 	 * Load and stock images
 	 */
 	static preload() {
-		Game.setup();
+		let _onAssetsLoaded = function(e, ressources) {
+			Game.assets = ressources;
+
+			Game.setup();
+		}
+
+		Game.loader.add(Game.assetsUrls);
+		Game.loader.add(Game.levels.urls);
+		Game.loader.load(_onAssetsLoaded);
 	}
 
 	/**
@@ -52,13 +85,52 @@ class Game {
 	 * Static setup method
 	 */
 	static setup() {
+		//Load the first level
+		Game.loadLevel(0, Game.assets.mapLvl1.data);
+
+
 		Game.gameLoop();
 	}
 
+	/**
+	 * Static gameLoop
+	 */
 	static gameLoop() {
+		requestAnimationFrame(Game.gameLoop);
+
 		//Check game status
 		Game.play();
 
 		Game.renderer.render(Game.stage.container);
+	}
+
+	/**
+	 * Display the current level (@TODO : Refactor index ?)
+	 *
+	 * @param int current index of the map
+	 * @param Object current map
+	 */
+	static loadLevel(index, map) {
+		Game.mapContainer = getTiledMapContainer(
+			{
+				dungeon_tiles : Game.assets.dungeon.texture,
+				collision : Game.assets.collision.texture,
+			},
+			map
+		);
+
+		Game.layers = {
+			collision : Game.mapContainer.getChildByName("collision")
+		}
+
+		//Set collision layer aplha to 0
+		Game.layers.collision.alpha = 0;
+
+		//Set scale and screen position
+		Game.mapContainer.scale.set(Game.stage.scaleModifier, Game.stage.scaleModifier)
+		Game.mapContainer.x = -(Game.levels.info[index].startX*Game.stage.scaleModifier)+(Game.stage.width/2);
+		Game.mapContainer.y = -(Game.levels.info[index].startY*Game.stage.scaleModifier)+(Game.stage.height/2);
+
+		Game.stage.container.addChild(Game.mapContainer);
 	}
 }
---
+++ www/js/tilemap.js
@@ -0,0 +1,106 @@
+/**
+ * Transform a tiledmap JSON to a PIXI container with layer and tiles
+ *
+ * Author: Teddy Kishi
+ *
+ * [TODO] handle animations
+ * [TODO] handle collisions
+ */
+let getTiledMapContainer = function(texturesArrNames, data){ 
+let computeLayer = function(layer){
+  // create tiles 2D array
+  let map = [];
+  for(let i = 0; i < layer.data.length; i+=layer.width){
+    let newMap = [];
+    for(let j = 0; j < layer.width; j++ ){
+      newMap.push( layer.data[i+j] )
+    }
+    map.push(newMap);
+  }
+  return map;
+}
+
+let computeTileset = function(tilesets){
+  // create tiles image 2D array
+  let count = 1, coords = {};
+
+  for(let i=0; i<tilesets.length; i++){
+
+    let tileset = tilesets[i],
+      columns = tileset.columns, // tileset.imagewidth / tileset.tilewidth;
+      rows = tileset.imageheight / tileset.tileheight;
+
+    if(tilesets[i].tilecount > 1){
+      for(let j=0; j<rows; j++){
+        for(let k=0; k<columns; k++){
+          coords[count] = { 
+            x :  k * tileset.tilewidth,
+            y :  j * tileset.tileheight,
+            name : tileset.name
+          };
+          count++;
+        }
+      }
+    } else {
+      coords[count] = { 
+          x :  0,
+          y :  0,
+          name : tileset.name
+        };
+        count++;
+       
+    }
+    
+    
+  }
+
+  return coords;
+}
+//
+let coords = computeTileset( data.tilesets );
+
+
+// draw on pixi
+let mapContainer = new PIXI.Container();
+
+for(let o = 0; o < data.layers.length; o++){
+  // set all the position fo each tiles
+  let map = computeLayer( data.layers[o] );
+  // create a main container  
+  let layerContainer = new PIXI.Container();
+  // set layer properties
+  layerContainer.alpha = data.layers[o].opacity;
+  layerContainer.visible = data.layers[o].visible;
+  layerContainer.x = data.layers[o].x;
+  layerContainer.y = data.layers[o].y;
+  // so we can grab it by his name
+  layerContainer.name = data.layers[o].name;
+  // create tiles
+  for(let rows=0; rows<map.length;rows++){
+    for(let cols=0; cols<map[rows].length;cols++){
+
+      let tile = coords[map[rows][cols]];
+
+       // add the sprite to the stage
+      if(tile){
+        let textureName = texturesArrNames[tile.name];
+        
+        let sprite = new PIXI.Sprite( 
+          new PIXI.Texture(
+            textureName, 
+            new PIXI.Rectangle(tile.x, tile.y, data.tilesets[0].tilewidth, data.tilesets[0].tileheight))
+        );
+        // position the sprite
+        sprite.x = cols * data.tilesets[0].tilewidth;
+        sprite.y = rows * data.tilesets[0].tileheight;
+        layerContainer.addChild(sprite);
+      }
+    }
+  }
+  // add the current layer to main container 
+  mapContainer.addChild(layerContainer);
+}
+
+return mapContainer;
+
+}
